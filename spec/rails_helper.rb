a489f8eacd822bf4f3a04167ec7e99de07c053e9
zaranah <zaranahier@gmail.com>
2022-01-20 20:44:07 +0900

    モデルの単体テストコード記入

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,8 +30,10 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+I18n.locale = "en"
+
 RSpec.configure do |config|
-  spec/rails_helper.rb
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
