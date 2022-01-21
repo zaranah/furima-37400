9631f308d7e8f25ee0f4c358ef0b2b70d60d1b58
zaranah <zaranahier@gmail.com>
2022-01-21 18:22:26 +0900

    テストコードの記入

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,79 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '出品できるとき' do
+      it 'name、info、category_id、sales_status_id、shipping_fee_status_id、prefecture_id、scheduled_delivery_id、price、imageが存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '出品できないとき' do
+      it 'nameが空では登録できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'infoが空では登録できない' do
+        @item.info = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Info can't be blank")
+      end
+      it 'categoryが空では登録できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'sales_statusが空では登録できない' do
+        @item.sales_status_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Sales status can't be blank")
+      end
+      it 'shipping_fee_statusが空では登録できない' do
+        @item.shipping_fee_status_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee status can't be blank")
+      end
+      it 'prefectureが空では登録できない' do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'scheduled_deliveryが空では登録できない' do
+        @item.scheduled_delivery_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Scheduled delivery can't be blank")
+      end
+      it 'priceが空では登録できない' do
+        @item.price, = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが全角では登録できない' do
+        @item.price = '５００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is invalid. Input half-width characters. Price is out of setting range")
+      end
+      it 'priceが299円以下では登録できない' do
+        @item.price = '100'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is invalid. Input half-width characters. Price is out of setting range")
+      end
+      it 'priceが9999999円以上では登録できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is invalid. Input half-width characters. Price is out of setting range")
+      end
+      it 'imageが空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+    end
+  end
+
 end
