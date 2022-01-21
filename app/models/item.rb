6540b936c6fe101eca74682425d533b6f8da2add
zaranah <zaranahier@gmail.com>
2022-01-21 20:34:49 +0900

    バリデーション任意修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,7 +19,6 @@ class Item < ApplicationRecord
   validates :scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank" }
 
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-  # validates :price, presence: true,
-  #                   inclusion: { in: 300..9_999_999, message: 'is invalid. Input half-width characters. Price is out of setting range' }
+  
   validates :image, presence: true
 end
