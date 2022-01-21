3daed2c4664154c89b78eda2796d4a8f4202fc60
zaranah <zaranahier@gmail.com>
2022-01-21 13:13:09 +0900

    Activehash作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,22 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
 
   belongs_to :user
   has_one :order
 
   validates :name, presence: true
   validates :info, presence: true
-  validates :category_id, presence: true
-  validates :sales_status_id, presence: true
-  validates :shipping_fee_status_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :scheduled_delivery_id, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :sales_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+
   validates :price, presence: true
 
 end
