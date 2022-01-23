75238fd00be49217433ddf6fa00a8e5ecef2f21c
zaranah <zaranahier@gmail.com>
2022-01-23 21:59:41 +0900

    アソシエーションの記入

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,5 @@
 class Order < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :prefecture
-  
   belongs_to :user
   belongs_to :item
   has_one :shipping
-
 end
