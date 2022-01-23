d030901aeab8887e479fcc086b1523e8ac84972d
zaranah <zaranahier@gmail.com>
2022-01-23 22:39:01 +0900

    Formオブジェクトクラス名修正

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -1,4 +1,4 @@
-class DonationAddress
+class OrderShipping
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :order_id
 
