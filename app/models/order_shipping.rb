21dd0e3ebed3ba459f83928a9afb95d1932c2e41
zaranah <zaranahier@gmail.com>
2022-01-23 22:16:41 +0900

    Formオブジェクトの作成

---
+++ app/models/order_shipping.rb
@@ -0,0 +1,20 @@
+class DonationAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :order_id
+
+  with_options presence: true do
+    validates :item_id
+    validates :user_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :addresses
+    validates :order_id
+    validates :phone_number, numericality: {with:  /\A\d{10}$|^\d{11}\z/, message: 'is invalid'}
+  end
+  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Shipping.create(postal_code: postal_code, prefecture: prefecture, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
+  end
+end
