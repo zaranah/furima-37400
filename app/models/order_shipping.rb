1998782ecd0c751fd02809e6c72cf126e06de645
zaranah <zaranahier@gmail.com>
2022-01-24 12:06:50 +0900

    バリデーションの修正

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -8,7 +8,7 @@ class OrderShipping
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :addresses
-    validates :phone_number, numericality: {only_integer: true, with: /\A\d{10}$|^\d{11}\z/, message: 'is invalid'}
+    validates :phone_number, numericality: {only_integer: true}, format: {with: /\A\d{10}$|^\d{11}\z/, message: "is invalid"}
     validates :token
   end
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
